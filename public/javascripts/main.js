533718f33ae3af390db6dd6aff2cb3c27a82272d
artseven <yabkevich.artsiom.09@gmail.com>
2017-06-05 02:18:41 -0400

    2 iteration  done

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,20 @@
 $("#option" ).click(function() {
   $( "#toggle" ).toggle( "slide" );
 });
+
+
+$(function() {
+
+// Dropdown toggle
+$('.dropdown-toggle').click(function(){
+  $(this).next('.dropdown').toggle();
+});
+
+$(document).click(function(e) {
+  var target = e.target;
+  if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+    $('.dropdown').hide();
+  }
+});
+
+});
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.post('/', (req, res, next) => {
+
+  res.render('index', {
+      input: req.body.quantity,
+      optionA: req.body.optionA,
+      optionB: req.body.optionB
+  });
+  console.log(req.body.quantity);
+  console.log(`optionSelector`+req.body.optionSelector);
+  console.log(req.body.optionA);
+  console.log(req.body.optionB);
+
+
+});
+
+
 module.exports = router;
