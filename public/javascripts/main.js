7278d65cd4bbc299fa9a9107b88f88c6247282a9
artseven <yabkevich.artsiom.09@gmail.com>
2017-06-05 14:56:26 -0400

    Final

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,20 @@
-$("#option" ).click(function() {
-  $( "#toggle" ).toggle( "slide" );
-  $( "#slide" ).addClass('active');
+// $("#option" ).click(function() {
+//   $( "#toggle" ).toggle( "slide" );
+// });
+
+$(document).ready(function(){
+    $('#option').click(function(event){
+        event.stopPropagation();
+         $("#toggle").toggle("slide");
+    });
+    $("#toggle").on("click", function (event) {
+        event.stopPropagation();
+    });
 });
 
-$(document).click(function(e) {
-  if ($("slide").hasClass('active')) {
-    $( "#toggle" ).toggle( "slide" );
-  }
+$(document).on("click", function () {
+    $("#toggle").hide();
 });
-
 
 
 $(function() {
@@ -27,73 +33,21 @@ $(document).click(function(e) {
 
 });
 // ---------------JS--------------------
-// Pagination
-var list = new Array();
-   var pageList = new Array();
-   var currentPage = 1;
-   var numberPerPage = 3;
-   var numberOfPages = 0;
+// Creating JSON to use data among multiple pages
+$('.submit').click(function(){
+    var json = {};
+    json.items = [];
+    // $('selector').each(function(e){
+    //     var obj = {};
+    //     obj.quantity = $(this).find('input[name=name]').val();
+    //     // obj.url = $(this).find('input[name=url]').val();
+    //     json.items.push(obj);
+    // });
 
-function makeList() {
-   for (x = 0; x < 200; x++)
-       list.push(x);
-   numberOfPages = getNumberOfPages();
-}
 
-function getNumberOfPages() {
-   return Math.ceil(list.length / numberPerPage);
-}
-
-function nextPage() {
-   currentPage += 1;
-   loadList();
-}
-
-function previousPage() {
-   currentPage -= 1;
-   loadList();
-}
-
-function firstPage() {
-   currentPage = 1;
-   loadList();
-}
-
-function lastPage() {
-   currentPage = numberOfPages;
-   loadList();
-}
-
-function loadList() {
-   var begin = ((currentPage - 1) * numberPerPage);
-   var end = begin + numberPerPage;
-
-   pageList = list.slice(begin, end);
-   drawList();
-   check();
-}
-
-function drawList() {
-   document.getElementById("list").innerHTML = "";
-   for (r = 0; r < pageList.length; r++) {
-       document.getElementById("list").innerHTML += pageList[r] + "";
-   }
-}
-
-function check() {
-   document.getElementById("next").disabled = currentPage == numberOfPages ? true : false;
-   document.getElementById("previous").disabled = currentPage == 1 ? true : false;
-   document.getElementById("first").disabled = currentPage == 1 ? true : false;
-   document.getElementById("last").disabled = currentPage == numberOfPages ? true : false;
-}
-
-function load() {
-   makeList();
-   loadList();
-}
-
-window.onload = load;
-
+    // console.log(json);
+});
+//----------- Pagination
 // Quantity-input form validation
 function validateForm() {
     var x = document.forms['myForm']['quantity'].value;
